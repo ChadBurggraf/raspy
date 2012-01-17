23925a9db5bce4f2c9f598c4c033fa98984e2c07
Chad Burggraf <chad.burggraf@gmail.com>
2012-01-17 10:23:28 -0700

    Initial project setup.

---
+++ Source/Raspy.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Tasty Codes">
+//     Copyright (c) 2012 Chad Burggraf.
+// </copyright>
+//-----------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Raspy Test")]
+[assembly: AssemblyDescription("Raspy test suite.")]
+[assembly: Guid("8bbf2ee7-8959-4b73-9416-bbd7fe6ef0ac")]
---
+++ Source/Raspy/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Tasty Codes">
+//     Copyright (c) 2012 Chad Burggraf.
+// </copyright>
+//-----------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Raspy")]
+[assembly: AssemblyDescription("Raspy arithmetic parsing and evaluation library.")]
+[assembly: Guid("783a6de0-c40c-40cc-b094-93a945eb036c")]
